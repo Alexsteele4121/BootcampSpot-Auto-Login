f5df3fb0331d3e198217fd6da3fac3794808081d
Alexsteele4121 <63519706+Alexsteele4121@users.noreply.github.com>
2021-10-25 17:07:49 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ creds = {
         "username": "YOUR USERNAME",
         "password": "YOUR PASSWORD"
     }
-
 def internet_active() -> bool:
     connection = http.HTTPConnection("www.google.com", timeout=5)
     try:
@@ -25,6 +24,29 @@ def internet_active() -> bool:
     except:
         connection.close()
         return False
+
+
+def sign_in(browser: Chrome, username: str, password: str):
+    browser.get_page('https://bootcampspot.com/login')
+    browser.enter_text('/html/body/div/section/div/div[2]/input[1]', username)
+    browser.enter_text('/html/body/div/section/div/div[2]/input[2]', password)
+    browser.press_button('/html/body/div/section/div/div[2]/button')
+    time.sleep(4)
+
+
+def complete_survey(browser: Chrome):
+    browser.press_button('/html/body/div/main/div/section/div/div/div/button')
+    time.sleep(1)
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[1]/fieldset/div/label[5]/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[2]/fieldset/div/label[3]/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[3]/fieldset/div/label[5]/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[4]/fieldset/div/label[5]/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[5]/fieldset/div/label[5]/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[6]/fieldset/div/label[5]/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[7]/fieldset/div/label[5]/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[8]/fieldset/div/label[6]/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/ol/li[9]/fieldset/div/ul/li[5]/label/input')
+    browser.press_button('/html/body/div/main/div/section/div/div/form/div/div/button')
 
 
 def main():
@@ -42,17 +64,21 @@ def main():
     browser = Chrome(chrome_driver, browser_options)
 
     inform('Signing into bootcampspot...')
-    browser.get_page('https://bootcampspot.com/login')
-    browser.enter_text('/html/body/div/section/div/div[2]/input[1]', creds['username'])
-    browser.enter_text('/html/body/div/section/div/div[2]/input[2]', creds['password'])
-    browser.press_button('/html/body/div/section/div/div[2]/button')
-    time.sleep(3)
+    sign_in(browser, creds['username'], creds['password'])
 
-    if browser.get_current_url() != 'https://bootcampspot.com/':
+    current_url = browser.get_current_url()
+    if current_url == 'https://bootcampspot.com/login':
         error('Sign in was unsuccessful. Exiting script...')
         browser.close()
         exit(1)
+
     successful('Sign in was successful!')
+
+    if 'feedback' in current_url.lower():
+        inform('Completing the weekly survey.')
+        complete_survey(browser)
+        browser.get_page('https://bootcampspot.com/')
+        time.sleep(4)
 
     check_in_status = browser.get_text('/html/body/div/main/div/section/div/div[3]/div/div[3]/ul/li[2]')
     if check_in_status == 'Check In begins 45 minutes before Class':
@@ -61,17 +87,17 @@ def main():
         exit(0)
     elif check_in_status == 'Check In To Class':
         browser.press_button('/html/body/div/main/div/section/div/div[3]/div/div[3]/ul/li[2]/a')
-    check_in_status = browser.get_text('/html/body/div/main/div/section/div/div[3]/div/div[3]/ul/li[2]')
+        time.sleep(1)
+        check_in_status = browser.get_text('/html/body/div/main/div/section/div/div[3]/div/div[3]/ul/li[2]')
+
     if check_in_status == 'You are currently checked in as present':
         successful(check_in_status)
-        if 'y' in input('[?] Would you like to join class? (y/n): ').lower():
+        if 'y' in question('Would you like to join class? (y/n): ').lower():
             url = browser.get_element_url('/html/body/div/main/div/section/div/div[2]/div/div/div/a')
             os.startfile(url)
             inform(url)
     else:
         error('Unable to check in! \'Check in\' element not found.')
-        browser.close()
-        exit(1)
 
     browser.close()
     exit(0)
